495841da8e5e653115f2fb8c3c4d44257ee03f4e
GVNZM <113910179+GVNZM@users.noreply.github.com>
2022-09-19 10:14:09 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,13 +4,13 @@
 import streamlit as st
   
 # Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Meu prof. é Incrível!")
+st.title("GIAN E GIOVANNI")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
+st.header("GIAN E GIOVANNI QUEM SÃO?")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
+st.subheader("NOVA DUPLA SERTANEJO DE SUCESSO DA ETEC BENTO QUIRINO")
 
 # Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
+st.write("A dupla sertaneja teve início em 2020, com uma parada na turnê em 2020-2021 devido à pandemia, porém em 2022 voltaram com tudo e dobram cache 'Nunca vi tanto dinheiro na minha vida' Diz Giovanni ")
